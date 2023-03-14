07c6bbd662e12d3886fa922effc05c06d54707af
pg1219 <peter.gronseth@gmail.com>
2023-03-13 23:44:00 -0400

    first push

---
+++ script.js
@@ -0,0 +1,46 @@
+// const questions = [
+//     {
+
+//     question1: "A very useful tool used during development and debugging for printing content to the debugger is:",
+
+//         option1: "terminal/bash",
+//         option2: "Javascript",
+//         option3: "for loops",
+//         option4: "console.log", 
+//         correctOption: "option2"
+//     },
+//     {
+//     question2: "Arrays in JavaScript can be used to store _____:",
+
+//         option1: "numbers and strings",
+//         option2: "other Arrays",
+//         option3: "booleans",
+//         option4: "all of the above",
+//         correctOption: "option4"
+//     }
+//     {
+//     question3: "Commonly used data types DO NOT include:",
+
+//         option1: "strings",
+//         option2: "booleans",
+//         option3: "alerts",
+//         option4: "numbers",
+//         correctOption: "option3"
+//     }
+//     {
+//     question4: "The condition in an if/else statement is enclosed within ______:"
+//         option1: "quotes",
+//         option2: "curly brackets",
+//         option3: "parentheses",
+//         option4: "square brackets",
+//         correctOption: "option3"
+//     }
+//     {
+//     question5: "String values must be enclosed within ____ when being assigned to variables."
+//         option1: "quotes",
+//         option2: "curly brackets",
+//         option3: "commas",
+//         option4: "parentheses",
+//         correctOption: "option1"
+
+//     }]
