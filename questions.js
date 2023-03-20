18dce33a126f3a9ec7c045ccf243d6fd2d2ec47d
pg1219 <peter.gronseth@gmail.com>
2023-03-19 22:59:24 -0400

    sunday night push

---
+++ questions.js
@@ -0,0 +1,34 @@
+const questions = [
+  {
+    question:
+      "A very useful tool used during development and debugging for printing content to the debugger is:",
+    choices: ["terminal/bash", "Javascript", "for loops", "console.log"],
+    answer: "Javascript",
+  },
+  {
+    question:
+    "Arrays in JavaScript can be used to store _____:",
+    choices: ["numbers and strings", "other Arrays", "booleans", "all of the above"],
+    answer: "all of the above",
+  },
+  {
+    question:
+    "Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts",
+  },
+  {
+    question:
+    "The condition in an if/else statement is enclosed within ______:",
+    choices: ["quotes",  "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses",
+  },
+  {
+    question:
+    "String values must be enclosed within ____ when being assigned to variables.",
+    choices: ["quotes", "curly brackets", "commas", "parentheses"],
+    answer: "quotes",
+  },
+];
+
+
--- script.js
+++ script.js
@@ -1,46 +1,64 @@
-// const questions = [
-//     {
+var questionsEl = document.querySelector("#questions");
+var timerEl = document.querySelector("#time");
+var choicesEl = document.querySelector("#choices");
+var submitBtn = document.querySelector("#submit");
+var startBtn = document.querySelector("#start");
+var initialsEl = document.querySelector("#initials");
+var feedbackEl = document.querySelector("#feedback")
 
-//     question1: "A very useful tool used during development and debugging for printing content to the debugger is:",
+var currentQuestionIndex = 0;
+var time = questions.length * 15;
+var timerId;
 
-//         option1: "terminal/bash",
-//         option2: "Javascript",
-//         option3: "for loops",
-//         option4: "console.log", 
-//         correctOption: "option2"
-//     },
-//     {
-//     question2: "Arrays in JavaScript can be used to store _____:",
+// startButton.addEventListener("click", function() {
+//   startQuiz();
+// });
 
-//         option1: "numbers and strings",
-//         option2: "other Arrays",
-//         option3: "booleans",
-//         option4: "all of the above",
-//         correctOption: "option4"
-//     }
-//     {
-//     question3: "Commonly used data types DO NOT include:",
 
-//         option1: "strings",
-//         option2: "booleans",
-//         option3: "alerts",
-//         option4: "numbers",
-//         correctOption: "option3"
-//     }
-//     {
-//     question4: "The condition in an if/else statement is enclosed within ______:"
-//         option1: "quotes",
-//         option2: "curly brackets",
-//         option3: "parentheses",
-//         option4: "square brackets",
-//         correctOption: "option3"
-//     }
-//     {
-//     question5: "String values must be enclosed within ____ when being assigned to variables."
-//         option1: "quotes",
-//         option2: "curly brackets",
-//         option3: "commas",
-//         option4: "parentheses",
-//         correctOption: "option1"
+function startQuiz() {
+  var startScreenEl = document.getElementById("start-screen");
+  startScreenEl.setAttribute("class", "hide");
 
-//     }]
+  questionsEl.removeAttribute("class");
+
+  timerId = setInterval(clockTick, 1000);
+
+  timerEl.textContent = time;
+
+  getQuestion();
+}
+
+function clockTick() {
+  time--;
+  timerEl.textContent = time;
+
+  if (time <= 0) {
+    console.log("Quiz is over!");
+  }
+}
+
+
+
+function getQuestion() {
+  var currentQuestion = questions[currentQuestionIndex];
+
+  var titleEl = document.getElementById("question-title");
+  titleEl.textContent = currentQuestion.title;
+
+  choicesEl.innerHTML = "";
+
+  currentQuestion.choices.forEach(function (choice, i) {
+
+    var choiceNode = document.createElement("button");
+    choiceNode.setAttribute("class", "choice");
+    choiceNode.setAttribute("value", choice);
+
+    choiceNode.textContent = i + 1 + ". " + choice;
+
+    // attach click event listener to each choice
+    choiceNode.onclick = questionClick;
+
+    // display on the page
+    choicesEl.appendChild(choiceNode);
+  });
+}
